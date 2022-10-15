fab3a300e6c98279902f98d9e579bdd7531ad1f5
Jorsh <113651771+Jorshuaa@users.noreply.github.com>
2022-10-15 23:38:35 +0100

    Add files via upload
    portfolio

---
+++ index.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <header style="background-color:white;">
+        <div class="logo">
+            <img src="images/logo.png" alt="logo">
+        </div>
+        <nav>
+            <ul>
+                <li><a href="#">Projects</a></li>
+                <li><a href="#">Skills</a></li>
+                <li><a href="#">About</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <div class="container-1">
+            <div class="left">
+                <h1>Hey there, &#128075</h1>
+                <h2>Am Jorsh and I am a </h2>
+                <h3>Front-end Web Developer</h3>
+                <div class="buttons">
+                    <button><a href="#">Hire Me</a></button>
+                    <button><a href="#">View my resume</a></button>
+                </div>
+            </div>
+            <div class="right">
+                <img class="imagery" src="images/moi.png" alt="moi">
+            </div>
+        </div>
+        <div class="container-2">
+            <div class="my-p-text">
+            <h1>My Projects</h1>
+</div>
+            <div class="projects">
+                <img class="image" src="images/fylo.png"/>
+                <img class="image" src="images/tallbuilding.jpg"/>
+                <img class="image" src="images/moi.png"/>
+                <img class="image" src="images/tallbuilding.jpg"/>
+                <img class="image" src="images/newbg.png"/>
+                <img class="image" src="images/guynew2.png"/>
+                <img class="image" src="images/tallbuilding.jpg"/>
+                <img class="image" src="images/fylo.png"/>
+            </div>
+        </div>
+        <!-- <div class="container-3">
+            <h1>My Skills</h1>
+            <div class="skills">
+                <p>Html</p>
+                <progress value="95" max="100"></progress>
+            </div>
+            <div class="skills">
+                <p>Css</p>
+                <progress value="80" max="100"></progress>
+            </div>
+            <div class="skills">
+                <p>Javascript</p>
+                <progress value="30" max="100"></progress>
+            </div>
+            <div class="skills">
+                <p>React.js</p>
+                <progress value="10" max="100"></progress>
+            </div>
+            <div class="skills">
+                <p>Python</p>
+                <progress value="40" max="100"></progress>
+            </div>
+        </div> -->
+        <!-- <div class="container-4">
+            <h1>About Moi</h1>
+            <div class="aboutme">
+                <p>Holla, My name is Jorsh, a frontend web developer based in Nigeria.
+                    Wanna have a chat with me, fill in the form below &#128071;
+                </p>
+            </div>
+            <div class="contact">
+                <form method="POST"></form>
+            </div>
+        </div> -->
+    </main>
+    <!-- <script>
+           var i = 0;
+           for (; i < 10; i++) {
+            document.write(i);
+           } 
+    </script> -->
+</body>
+</html>
+<!-- Fylo is a responsive website for storing all imoortant files in one
+                        secure location to be accessed anytime anywhere. -->
